fe22323eff0bd160c34e0f17e1e6dfe1f8426dff
Devsethi3 <devsethi4522@gmail.com>
2024-06-29 19:51:08 +0530

    Implemented Authentication

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,9 @@ const LoginPage = () => {
   return (
     <div className="w-full flex flex-col bg-gradient-to-b from-blue-100 dark:from-emerald-900 to-yellow-100 dark:to-violet-900 items-center h-screen justify-center">
       <Link href="/">
-        <Image src="/images/logo.png" width="50" height="50" alt="logo" />
+        <span className="text-2xl font-bold bg-gradient-to-r from-emerald-300 to-blue-400 text-transparent bg-clip-text">
+          ArtiGen
+        </span>
       </Link>
       <h2 className="text-2xl my-2 text-center font-semibold">
         Sign in to your account
