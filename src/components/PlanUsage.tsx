0241547dde75de797b5c91b22da26122557161bf
Devsethi3 <devsethi4522@gmail.com>
2024-07-06 19:58:55 +0530

    Update PlanUsage.tsx

--- src/components/PlanUsage.tsx
+++ src/components/PlanUsage.tsx
@@ -37,7 +37,7 @@ const PlanUsage: React.FC = () => {
       intervalId = setInterval(fetchData, 10000);
     }
 
-    // Cleanup interval on
+    // Cleanup 
     return () => clearInterval(intervalId);
   }, [user]);
 
