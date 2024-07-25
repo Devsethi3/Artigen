bd2c306951e4a6b97a1f762e782fe59db5521f92
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-07-25 22:23:34 +0530

    Update SidebarHeader.tsx

--- src/app/dashboard/_component/SidebarHeader.tsx
+++ src/app/dashboard/_component/SidebarHeader.tsx
@@ -47,7 +47,7 @@ const SidebarHeader = () => {
   const userButtonAppearance = {
     elements: {
       userButtonAvatarBox: "w-10 h-10",
-      userButtonPopoverCard: "bg-blue-100", // Custom background for the popover card
+      userButtonPopoverCard: "bg-blue-100", // Custom background for the popover
       userButtonPopoverActionButton: "text-red-600", // Custom text color for action buttons
     },
   };
