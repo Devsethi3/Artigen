5d7593f0ca2bbe8d458077153f0685ab50deaab7
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-07-30 22:32:54 +0530

    Update FormSection.tsx

--- src/app/dashboard/content/_component/FormSection.tsx
+++ src/app/dashboard/content/_component/FormSection.tsx
@@ -45,7 +45,7 @@ const FormSection: React.FC<FormSectionProps> = ({
       required: item.required,
       onChange: handleInputChange,
       value: formData[item.name] || "",
-      placeholder: item.placeholder || "", // 
+      placeholder: item.placeholder || "",
       className: "w-full",
     };
 
