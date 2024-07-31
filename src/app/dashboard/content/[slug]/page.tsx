ef41b866d2cd315f5f8ad11c73feba0b3adfb6a1
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-07-31 22:23:29 +0530

    Update page.tsx

--- src/app/dashboard/content/[slug]/page.tsx
+++ src/app/dashboard/content/[slug]/page.tsx
@@ -58,7 +58,7 @@ const CreatePage = ({ params }: CreatePageProps) => {
       const selectedPrompt = selectedTemplate?.aiPrompt || "";
       const finalAIPrompt = `${JSON.stringify(formData)}, ${selectedPrompt}`;
 
-      // Here we're calling the actual AI 
+      // Here we're calling the actual 
       const result = await chatSession.sendMessage(finalAIPrompt);
       const responseText = await result.response.text();
 
