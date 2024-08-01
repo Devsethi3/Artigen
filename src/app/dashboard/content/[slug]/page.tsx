e0bea98e11902e17c496a5b238ab6929ced99584
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-08-01 21:53:37 +0530

    Update page.tsx

--- src/app/dashboard/content/[slug]/page.tsx
+++ src/app/dashboard/content/[slug]/page.tsx
@@ -58,7 +58,7 @@ const CreatePage = ({ params }: CreatePageProps) => {
       const selectedPrompt = selectedTemplate?.aiPrompt || "";
       const finalAIPrompt = `${JSON.stringify(formData)}, ${selectedPrompt}`;
 
-      // Here 
+      // 
       const result = await chatSession.sendMessage(finalAIPrompt);
       const responseText = await result.response.text();
 
