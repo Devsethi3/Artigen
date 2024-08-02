b099468ed1989854de9573c7ce77fca2fc5300a3
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-08-02 22:36:32 +0530

    Update page.tsx

--- src/app/dashboard/content/[slug]/page.tsx
+++ src/app/dashboard/content/[slug]/page.tsx
@@ -61,7 +61,7 @@ const CreatePage = ({ params }: CreatePageProps) => {
       const result = await chatSession.sendMessage(finalAIPrompt);
       const responseText = await result.response.text();
 
-      // Update the aiResult with
+      // Update the aiResult
       setAiResult(responseText);
 
       // Saving Data in DB
