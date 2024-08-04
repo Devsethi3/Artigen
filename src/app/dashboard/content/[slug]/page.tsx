12b448f54c3707875ede73a0802a650e05cf51e3
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-08-04 23:08:56 +0530

    Update page.tsx

--- src/app/dashboard/content/[slug]/page.tsx
+++ src/app/dashboard/content/[slug]/page.tsx
@@ -69,7 +69,7 @@ const CreatePage = ({ params }: CreatePageProps) => {
         responseText
       );
 
-      // Update
+      // 
       setTotalUsage((prev: any) => prev + responseText.length);
     } catch (error) {
       console.error("Error generating AI content:", error);
